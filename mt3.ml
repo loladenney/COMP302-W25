9a896bcccaa5d22b53c51e067e84444f0f8d14c4
Lola Denney <lolacdenney@gmail.com>
2025-12-01 11:09:45 -0500

    fixed typo

--- mt3.ml
+++ mt3.ml
@@ -14,31 +14,11 @@ type exp =
 | ConstI of int
 | Var of ident                      (* x *)
 | Fn of ident * tp * exp            (* fn x : t -> e  *)
-
-
-(* Parsing *)
-
-(*
-Let's try to parse these examples: 
-1. fun (x : Susp Int) -> let delay y = x in y
-
-2. fun (x : int => int) -> fun (y: int) -> 5 
-
-3. let delay x = delay (fun (y:int) -> y) in x
-*)
+| Delay of exp                      (* delay e *)
+| LetDelay of ident * exp * exp     (* let delay x = e1 in e2 *)
 
 
 
-(*       Gamma |- e :  A 
-    --------------------------delay
-    Gamma |- delay e : Susp A
-
-
-    Gamma |- e1 : Susp A    Gamma,x: A |- e2 : B
-    -------------------------------------------- letdelay
-        Gamma |- let delay x = e1 in e2 : B
-       
-*)
 
 (*Type Inference*)
 exception TypeError of string
@@ -51,8 +31,22 @@ let rec infer (c : ctx) (e: exp) : tp =
     | None -> raise (TypeError "unbound variable")
     | Some t -> t)
   | Fn (x, t, e) -> Arrow( t, infer ((x, t):: c) e)
+  | Delay e -> Susp (infer c e)
+  | LetDelay (x, e1,e2) -> 
+      begin match infer c e1 with
+      | Susp a -> infer ((x,a)::c) e2
+      | _ -> raise (TypeError "LetDelay requires e1 be of type Susp a")
+      end
 
+(*       Gamma |- e :  A 
+    --------------------------delay
+    Gamma |- delay e : Susp A
 
+    Gamma |- e1 : Susp A    Gamma,x: A |- e2 : B
+    -------------------------------------------- letdelay
+        Gamma |- let delay x = e1 in e2 : B
+       
+*)
 
 
 
@@ -72,25 +66,28 @@ let testinfer3 = infer [] (LetDelay ("x", Delay (Fn ("y", Int, Var "y")), Var "x
 
 (* Evaluation *)
 
+
+(* outputs the result of [d/z]e *)
+let rec subst ((d, z) : exp * ident) (e : exp) : exp = failwith "imagine this is implemented"
+exception EvaluationStuck
+let rec eval (e : exp): exp =
+  match e with 
+  | Var _ -> raise EvaluationStuck
+  | ConstI i -> e
+  | Fn (_,_,_) -> e         (* functions are values *)
+  | Delay e' -> Delay e'
+  | LetDelay (x, e1, e2) -> 
+    (match  eval e1 with
+    | Delay e1' -> eval (subst (e1',x) e2)
+    | _ -> raise EvaluationStuck
+    )
+
 (*
    e1 || delay e1'   [e1'/x]e2 || v
    -------------------------------- letdelay     --------------------- delay
    let delay x = e1 in e2 || v                    delay e || delay e
 
 *)
-
-(* outputs the result of [d/z]e *)
-let rec subst ((d, z) : exp * ident) (e : exp) : exp = failwith "imagine this is implemented"
-
-exception EvaluationStuck
-
-let rec eval (e : exp): exp =
-  match e with 
-  | Var _ -> raise EvaluationStuck
-  | ConstI i -> e
-  | Fn (_,_,_) -> e         (* functions are values *)
-
-
 
 
 
